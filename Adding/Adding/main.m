50f1494a25ba2d3fea14251fc4aebdcc5b08309c
Tejas Mehta <tj@tejas-mehta.com>
2020-05-03 19:00:18 -0700

    pretty easy lol

--- Adding/Adding/main.m
+++ Adding/Adding/main.m
@@ -10,8 +10,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int firstNum = 2;
+        int secondNum = 3;
+        float firstFloat = 2.2;
+        float secondFloat = 5.2;
+        int totalSum = firstNum + secondNum;
+        int totalSubtraction = firstNum - secondNum;
+        float totalFloat = firstFloat*firstFloat;
+        float totalFloatSubtraction = firstFloat - secondFloat;
+        
+        NSLog(@"The sum of %d and %d is %d", firstNum, secondNum, totalSum);
+        NSLog(@"The subtraction of %d and %d is %d", firstNum, secondNum, totalSubtraction);
+        NSLog(@"The square of %f is %f", firstFloat, totalFloat);
+        NSLog(@"The subtraction of %f and %f is %f", firstFloat, secondFloat, totalFloatSubtraction);
     }
     return 0;
 }
