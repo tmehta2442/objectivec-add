db934ba06a22abe8b6511cc694c29a370523853c
Tejas Mehta <tj@tejas-mehta.com>
2020-05-03 18:46:24 -0700

    create new MacOS app

---
+++ Adding/Adding/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Adding
+//
+//  Created by Tejas Mehta on 5/3/20.
+//  Copyright © 2020 Tejas Mehta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
